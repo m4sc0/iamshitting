662a49f9ba49cfd2ff1c6174bb6bb7e5ec188e6f
Philip Löbl <contact.masco@gmail.com>
2025-10-14 00:12:00 +0200

    build test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <main style="display:grid;place-items:center;height:100vh">
         <h1>Hello from the dev env of iamshitting.com</h1>
-        <p>Created with &lt;3</p>
+        <p>Created with &lt;3 by Masco and Jeratomb</p>
     </main>
 </template>
